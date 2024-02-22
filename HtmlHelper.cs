2df3fe4c10859518b2f3a085da84b2ca20f61ee5
Shulamit-Rubinstin <yr8410@gmail.com>
2024-02-22 14:23:18 +0200

    first commit

---
+++ HtmlHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace Prakticode2
+{
+    internal class HtmlHelper
+    {
+        private readonly static HtmlHelper _instance=new HtmlHelper();
+        public static HtmlHelper Instance { get { return _instance; } }
+        public string[] Tags { get; set; }
+        public string[] HtmlVoidTags { get; set; }
+        private HtmlHelper()
+        {
+                string json1 = File.ReadAllText("jsonFiles/HtmlTags.json");
+                string json2 = File.ReadAllText("jsonFiles/HtmlVoidTags.json");
+
+            Tags = JsonSerializer.Deserialize<string[]>(json1);
+            HtmlVoidTags = JsonSerializer.Deserialize<string[]>(json2);
+        }
+    }
+}
